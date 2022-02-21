0bda55669fa70fc410ad66d0569f21d2cad16c43
Evan Tan <tcyevan@gmail.com>
2022-02-21 12:31:58 +0800

    feat: wallet connect button disappears after connect

--- src/Wallet.js
+++ src/Wallet.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { utils } from "ethers";
 
 const Wallet = () => {
-    const [errorMessage, setErrorMessage] = useState(null);
     const [defaultAccount, setDefaultAccount] = useState(null);
     const [userBalance, setUserBalance] = useState(null);
     const [connButtonText, setConnButtonText] = useState("Connect Wallet");
@@ -11,32 +10,36 @@ const Wallet = () => {
 
     const connectWalletHandler = async () => {
         // provider = new ethers.providers.Web3Provider(window.ethereum);
-        if (provider) {
+
+        if (!provider) {
+            alert("Get MetaMask!");
+            return;
+        }
+        try {
             const accounts = await provider.request({
                 method: "eth_requestAccounts",
             });
             accountHandler(accounts[0]);
+            // unused
+            setConnButtonText("Connected");
             const chainId = await provider.request({ method: "eth_chainId" });
             if (chainId !== "0x4") {
-                setErrorMessage("Not on Rinkeby!");
+                alert("Switch to Rinkeby network!");
             }
-
-            setConnButtonText("Connected");
-        } else {
-            setConnButtonText("Not Connected");
-            setErrorMessage("Install MetaMask");
+        } catch (error) {
+            console.log(error);
         }
     };
 
     const accountHandler = (_account) => {
         setDefaultAccount(_account);
-        getUserBalance(_account.toString());
+        getUserBalance(_account);
     };
 
     const getUserBalance = async (_address) => {
         const weiBalance = await provider.request({
             method: "eth_getBalance",
-            params: [_address, "latest"],
+            params: [_address.toString(), "latest"],
         });
         const etherBalance = utils.formatEther(weiBalance);
         setUserBalance(etherBalance);
@@ -44,20 +47,31 @@ const Wallet = () => {
 
     // reload window on changing networks
     const chainHandler = (_chainId) => {
-        // window.location.reload();
-        setErrorMessage("Not on the Rinkeby network!");
+        window.location.reload();
     };
 
     // listen for account changes
-    provider.on("accountsChanged", accountHandler);
-    provider.on("chainChanged", chainHandler);
+    if (provider) {
+        provider.on("accountsChanged", accountHandler);
+        provider.on("chainChanged", chainHandler);
+    }
+
+    const renderAccountDetails = () => (
+        <div>
+            <div>Address: {defaultAccount}</div>
+            <div>Balance: {userBalance}</div>
+        </div>
+    );
+
+    const renderConnectButton = () => (
+        <button onClick={connectWalletHandler}>{connButtonText}</button>
+    );
 
     return (
         <div className="Wallet">
-            <button onClick={connectWalletHandler}>{connButtonText}</button>
-            <div>Address: {defaultAccount}</div>
-            <div>Balance: {userBalance}</div>
-            <div>{errorMessage}</div>
+            {defaultAccount === null
+                ? renderConnectButton()
+                : renderAccountDetails()}
         </div>
     );
 };
