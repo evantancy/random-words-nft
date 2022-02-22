2012d79f304800fe7a61a4b3177822e739689496
Evan Tan <tcyevan@gmail.com>
2022-02-22 14:22:24 +0800

    feat: enable batch mint

--- src/Mint.js
+++ src/Mint.js
@@ -7,14 +7,19 @@ import { AddressContext } from "./AddressContext";
 
 const Mint = () => {
     const { address } = useContext(AddressContext);
-    const [mintAmount, setMintAmount] = useState(0);
+    const [mintAmount, setMintAmount] = useState(1);
 
     const CONTRACT_ADDRESS = "0x23bC2BeE10F8BDba5359F18dbA8d6E83b3067Bb8";
     const RINKEBY_URL = "https://rinkeby.etherscan.io";
 
+    // let currentSupply;
+    // let maxSupply;
+
     const toastOptions = {
         autoClose: 30000,
         position: toast.POSITION.BOTTOM_LEFT,
+        theme: "dark",
+        hideProgressBar: true,
     };
 
     const callMint = async () => {
@@ -31,7 +36,7 @@ const Mint = () => {
             );
 
             // call Solidity mint function
-            const tx = await contract.mint();
+            const tx = await contract.mint(mintAmount);
             toast(
                 <a
                     href={`${RINKEBY_URL}/tx/${tx.hash}`}
@@ -44,6 +49,8 @@ const Mint = () => {
             );
             // wait to be mined
             await tx.wait();
+            // currentSupply = await contract.currentSupply();
+            // maxSupply = await contract.maxSupply();
         } catch (error) {
             console.log(error);
         }
@@ -58,28 +65,38 @@ const Mint = () => {
         </div>
     );
 
-    // TODO: enable multiple minting
-    // const renderQuantityButton = () => (
-    //     <div>
-    //         <button
-    //             className="button-small"
-    //             onClick={() => setMintAmount(mintAmount - 1)}
-    //         >
-    //             -
-    //         </button>
-    //         {mintAmount}
-    //         <button
-    //             className="button-small"
-    //             onClick={() => setMintAmount(mintAmount + 1)}
-    //         >
-    //             +
-    //         </button>
-    //     </div>
-    // );
+    const renderQuantityButton = () => (
+        <div>
+            <button
+                className="button-small"
+                onClick={() => setMintAmount(mintAmount - 1)}
+            >
+                -
+            </button>
+            {mintAmount}
+            <button
+                className="button-small"
+                onClick={() => setMintAmount(mintAmount + 1)}
+            >
+                +
+            </button>
+        </div>
+    );
+
+    // const renderMintProgress = async () => {
+    //     return (
+    //         <div>
+    //             <p>
+    //                 {currentSupply.toString()} / {maxSupply.toString()} minted
+    //             </p>
+    //         </div>
+    //     );
+    // };
 
     return (
         <div className="Mint">
-            {/* {address === null ? "" : renderQuantityButton()} */}
+            {/* {address === null ? "" : renderMintProgress()} */}
+            {address === null ? "" : renderQuantityButton()}
             {address === null ? "" : renderMintButton()}
         </div>
     );
