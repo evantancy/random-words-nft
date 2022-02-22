f1ca3a040545c3f5d52abfdc89d7cf001f42a5c8
Evan Tan <tcyevan@gmail.com>
2022-02-22 09:45:22 +0800

    feat: add Hardhat deploy script

---
+++ hardhat.config.js
@@ -0,0 +1,45 @@
+require("@nomiclabs/hardhat-waffle");
+// require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+
+const secrets = require("./secrets.json");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    solidity: {
+        version: "0.8.1",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+                details: {
+                    yul: false,
+                },
+            },
+        },
+    },
+    networks: {
+        rinkeby: {
+            url: secrets.alchemyApiUrl,
+            accounts: [secrets.privateKey],
+        },
+    },
+    etherscan: {
+        apiKey: secrets.etherscanApiKey,
+    },
+};
