66c84ed35f8be67cf2bd0a300fe8c2622d9dd648
Evan Tan <tcyevan@gmail.com>
2022-03-05 20:28:01 +0800

    feat: add etherscan and opensea testnet links and logos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from "./assets/logo.png";
+import openseaLogo from "./assets/opensea-logo.svg";
+import etherscanLogo from "./assets/etherscan-logo.svg";
 import "./css/App.css";
 import React, { useState } from "react";
 import Wallet from "./Wallet";
@@ -33,6 +35,36 @@ const Header = () => (
     </header>
 );
 
+const Links = () => {
+    const CONTRACT_ADDRESS = "0x28446ba6fb0d8d0f0728576bdd7fb18b360030a5";
+    const RINKEBY_URL = "https://rinkeby.etherscan.io";
+    const TESTNET_URL =
+        "https://testnets.opensea.io/collection/random-words-gt0ei5sin3";
+
+    return (
+        <div className="links">
+            <a href={`${TESTNET_URL}`} target="_blank" rel="noreferrer">
+                <img
+                    src={openseaLogo}
+                    className="link-logo"
+                    alt="opensea link"
+                ></img>
+            </a>
+            <a
+                href={`${RINKEBY_URL}/address/${CONTRACT_ADDRESS}`}
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img
+                    src={etherscanLogo}
+                    className="link-logo"
+                    alt="etherscan link"
+                ></img>
+            </a>
+        </div>
+    );
+};
+
 const App = () => {
     const TWITTER_LINK = "https://twitter.com/nat_nave";
     return (
@@ -41,6 +73,7 @@ const App = () => {
             <Body />
             <div className="footer">
                 built by <a href={`${TWITTER_LINK}`}>@nat_nave</a>
+                <Links />
             </div>
         </div>
     );
